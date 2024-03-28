0c9a35909be3debdafb317f3626c91cd9761139b
oraculodata <oraculodata@gmail.com>
2024-03-28 08:17:13 -0300

    🟢

--- pages/2_👮‍♂️_Stop_and_Search.py
+++ pages/2_👮‍♂️_Stop_and_Search.py
@@ -2,14 +2,10 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-st.set_page_config(layout="wide")
-
-@st.cache_data()
 def load_data(csv_file):
     df = pd.read_csv(csv_file, sep=';', parse_dates=['date'])
     return df
 
-@st.cache_data()
 def filter_data(df, selected_months):
     if selected_months:
         filtered_df = df[df['date'].dt.strftime('%Y-%m').isin(selected_months)]
@@ -24,21 +20,28 @@ def plot_charts(df):
         color='gender',
         tooltip=[alt.Tooltip('gender', title='Gender'), alt.Tooltip('count()', title='Count')]
     ).properties(
-        width=450,
-        height=300
+        width=350,
+        height=250
     )
-    st.altair_chart(gender_chart)
-
+    
     age_chart = alt.Chart(df.dropna(subset=['age_range'])).mark_bar().encode(
         x='age_range',
         y='count()',
         color='age_range',
         tooltip=[alt.Tooltip('age_range', title='Age Range'), alt.Tooltip('count()', title='Count')]
     ).properties(
-        width=450,
-        height=300
+        width=350,
+        height=250
     )
-    st.altair_chart(age_chart)
+    
+    col1, col2 = st.columns(2)
+    with col1:
+        col1.subheader("Gender")
+        st.altair_chart(gender_chart)
+
+    with col2:
+        col2.subheader("Age Range")
+        st.altair_chart(age_chart)
 
     ethnicity_chart = alt.Chart(df.dropna(subset=['self_defined_ethnicity'])).mark_bar().encode(
         x='self_defined_ethnicity',
@@ -49,6 +52,8 @@ def plot_charts(df):
         width=750,
         height=500
     )
+
+    st.write("### Self Defined Ethnicity")
     st.altair_chart(ethnicity_chart)
 
     outcome_chart = alt.Chart(df.dropna(subset=['outcome'])).mark_bar().encode(
@@ -60,10 +65,12 @@ def plot_charts(df):
         width=750,
         height=500
     )
-    st.altair_chart(outcome_chart)
+
+    st.write("### Outcome")
+    st.altair_chart(outcome_chart)    
 
 def main():
-    st.write("# :cop: Individual stop and search records")
+    st.title(":cop: Individual stop and search records")
 
     csv_file = "data/stop_and_search.csv"
     df = load_data(csv_file)
--- 🛡️_Home.py
+++ 🛡️_Home.py
@@ -10,7 +10,7 @@ st.set_page_config(
 st.write("# :oncoming_police_car: Crimes in London")
 
 st.markdown(
-    """This project aims to assist those who plan to visit London or those who live in London. With this app, you can walk in the least risky areas to ensure your safety, avoiding places with higher security risks.
+    """This project aims to assist those who plan to visit City of London or those who live in City of London. With this app, you can walk in the least risky areas to ensure your safety, avoiding places with higher security risks.
 
 The data has been extracted from [https://data.police.uk/about](https://data.police.uk/about), so they are real data.
 """
